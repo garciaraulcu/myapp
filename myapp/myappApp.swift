ff4b733c627f41555cca9994ec0719de4b7bb8f7
garciaraulcu <garciaraulcu@gmail.com>
2023-03-15 11:25:23 -0600

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by CEDAM04 on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
